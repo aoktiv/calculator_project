0cb05a8075ecbd2307405cfae4b1b79396cafbe1
aoktiv <amdemidof2009@gmail.com>
2025-03-01 02:18:21 +0300

    Update orchestrator_test.go

--- orchestrator/orchestrator_test.go
+++ orchestrator/orchestrator_test.go
@@ -1,1 +1,56 @@
 package orchestrator
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCalcHandlerSuccessCase(t *testing.T) {
+	expected := `{"id":"1"}`
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(`{"expression":"2+2"}`))
+	w := httptest.NewRecorder()
+	calculateExpression(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if string(data) != expected {
+		t.Errorf("Expected %s but got %v", expected, string(data))
+	}
+}
+func TestCalcHandlerInvalidExpressionCase(t *testing.T) {
+	expected := `{"id":"2"}`
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(`{"expression":"2+2("}`))
+	w := httptest.NewRecorder()
+	calculateExpression(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if string(data) != expected {
+		t.Errorf("Expected %s but got %v", expected, string(data))
+	}
+}
+func TestCalcHandlerDivisionByZeroCase(t *testing.T) {
+	expected := `{"id":"3"}`
+	req := httptest.NewRequest(http.MethodGet, "/", bytes.NewBufferString(`{"expression":"2/0"}`))
+	w := httptest.NewRecorder()
+	calculateExpression(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if string(data) != expected {
+		t.Errorf("Expected %s but got %v", expected, string(data))
+	}
+}
